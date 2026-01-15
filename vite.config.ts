c78bc825044ea5e65141b70d249f43e3ac7a4fa3
jeffcyuen <jeffchengyuen@gmail.com>
2026-01-14 20:17:22 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/personal_portfolio/',   // <-- add this for GitHub Pages project site
+    base: '/',   // <-- add this for GitHub Pages project site
     server: {
       port: 3000,
       host: '0.0.0.0',
